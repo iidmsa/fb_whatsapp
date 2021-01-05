41429f9754e3894fab05f706a7ca251156404fe1
Fady Farag <42524948+iidmsa@users.noreply.github.com>
2021-01-04 23:01:06 -0600

    chat bubble

--- src/Chat.js
+++ src/Chat.js
@@ -1,259 +1,259 @@
-import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, DonutLarge, InsertEmoticon, Mic, MoreVert } from '@material-ui/icons';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import './Chat.css';
-import db from './firebase';
-import { useStateValue } from './StateProvider';
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  AttachFile,
+  DonutLarge,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+} from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Chat.css";
+import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 import firebase from "firebase";
-import { actionTypes } from './reducer';
+import { actionTypes } from "./reducer";
 
 function Chat(props) {
+  const [input, setInput] = useState("");
+  const [seed, setSeed] = useState("");
+  const [typing, setTyping] = useState([]);
+  const [idleWatch, setIdleWatch] = useState(null);
+  const { roomId } = useParams();
+  const [roomName, setRoomName] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [{ user, isTyping }, dispatch] = useStateValue();
 
-    const [input, setInput] = useState('');
-    const [seed, setSeed] = useState('');
-    var [docID, setDocId] = useState('test');
-    const [typing, setTyping] = useState(false);
-    const { roomId } = useParams();
-    const [roomName, setRoomName] = useState("");
-    const [messages, setMessages] = useState([]);
-    const [{ user, isTyping }, dispatch] = useStateValue();
+  const removeTyping = (room_id) => {
+    db.collection("rooms")
+      .doc(room_id || roomId)
+      .collection("typing")
+      .doc(user.displayName)
+      .delete();
+  };
 
-    useEffect (() => {
-        if (roomId) {
-            db.collection('rooms').doc(roomId).onSnapshot(snapshot => {
-                setRoomName(snapshot.data().name)
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => {
+          setRoomName(snapshot.data().name);
+        });
+
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("typing")
+        .onSnapshot((snapshot) => {
+          const typing = snapshot.docs.map((doc) => {
+            const id = doc.id;
+            const age = Date.now() - doc.data().lastEdit;
+            if (age < 10000) {
+              return id;
+            }
+            return "$";
+          });
+          setTyping(
+            typing.filter((name) => {
+              return name != user.displayName && name != "$";
             })
-
-            db.collection('rooms').doc(roomId)
-              .collection('messages')
-              .orderBy('timestamp', 'asc')
-              .onSnapshot(snapshot => (
-                  setMessages(snapshot.docs.map((doc) => doc.data()))
-              ));
-        }
-    }, [roomId]);
-
-    useEffect(() => {
-        setSeed(Math.floor(Math.random() * 500));
-    }, []);
-
-    useEffect(() => {
-
-        if (input != '') {
-            setTyping(true);
-            dispatch({
-                type: actionTypes.SET_TYPING,
-                isTyping: true,
-            });
-        } else {
-            dispatch({
-                type: actionTypes.SET_TYPING,
-                isTyping: false,
-            });
-        }
-
-
-        // db.collection('rooms').doc(roomId)
-        //     .collection('messages').add({
-        //         message: input,
-        //         name: user.displayName,
-        //         typing: true,
-        //         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        //     });
-
-
-    //    console.log(db.collection('rooms').doc().id);
-                   
-            //   setDocId( db.collection('rooms').doc );
-
-
-            // db.collection('rooms').doc(roomId)
-            //   .collection('messages')
-            //   .orderBy('timestamp', 'desc').limit(2)
-            //   .onSnapshot(snapshot => (
-            //     snapshot.docs.map((doc) => { 
-            //         console.log(doc.id);
-            //         if (doc.data().typing === true) {
-            //             setTyping(true);
-            //         } 
-            //         if (input === '' ) {
-            //             db.collection('rooms')
-            //             .doc(roomId)
-            //             .collection('messages')
-            //             .doc(doc.id).update({ typing: "false"  })
-            //             setTyping(false);
-            //         }
-                    
-            //     } ) 
-            // ));
-
-            // db.collection('rooms')
-            //   .doc(roomId)
-            //   .collection('messages')
-            //   .doc('7LGChzUW5OvbTRLHr2Fy').set({ name: "true"  })
-
-
-
-            //   .orderBy('timestamp', 'desc').limit(1).id );
-            //   .onSnapshot(snapshot => (
-            //       snapshot.docs.map((doc) => {  doc.data() ; console.log ( doc.set({ typing: false }))}  )  
-                  
-            // ));
-
-
-
-
-
-        
-              
-
-
-
-
-
-
-
-
-
-
-        console.log(input)
-        // db.collection('rooms')
-        //   .doc(roomId)
-        //   .collection('typing')
-        //   .doc('VjntTj6WAt2Iqr6H7Jbs').set({ flag: true  })
-
-
-        // db.collection('rooms').doc(roomId).collection('typing').onSnapshot(snapshot => {
-        //     snapshot.docs.map(doc => (
-        //      //  console.log(doc.data().flag),
-        //        setTyping(doc.data().flag),
-        //       console.log(doc.data().flag)
-        //     ))
-        // })
-
-        // console.log("input is changing");
-        // console.log(typing);
-    
-        // if (input === '') {
-
-            // console.log('input is empty');
-            // console.log(typing);
-
-            // db.collection('rooms')
-            //   .doc(roomId)
-            //   .collection('typing')
-            //   .doc('VjntTj6WAt2Iqr6H7Jbs').set({ flag: false  })
-
-            //   db.collection('rooms').doc(roomId).collection('typing').onSnapshot(snapshot => {
-            //     snapshot.docs.map(doc => (
-            //     //    console.log(doc.data().flag),
-            //        setTyping(doc.data().flag)
-            //     ))
-
-              //console.log(typing)
-            // })
-        // }
-
-
-
-        // db.collection('rooms').doc(roomId)
-        //       .collection('messages').add({
-        //         message: input,
-        //         name: user.displayName,
-        //         typing: false,
-        //         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        //       });
-
-
-
-
-
-    }, [input]);
-
-    const sendMessage = (e) => {
-        e.preventDefault();
-        console.log(input);
-
-
-        db.collection('rooms').doc(roomId)
-              .collection('messages').add({
-                message: input,
-                name: user.displayName,
-                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-              });
-
-        setInput('');
+          );
+        });
+      console.log(typing);
+    }
+    return () => {
+      console.log(`Room ${roomId} has changed.`);
+      removeTyping(roomId);
     };
+  }, [roomId]);
 
-    return (
-        <div className="chat">
-            <div className="chat__header">
-                <Avatar src={`https://avatars.dicebear.com/4.5/api/human/${seed}.svg` }/>
-                <div className="chat__headerInfo">
-                    <h3>{roomName}</h3>
-                    <p> Last seen &nbsp; 
-                       { new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString() }
-                    </p>
-                </div>
-                <div className="chat__headerRight">
-                    <IconButton>
-                        <DonutLarge/>                        
-                    </IconButton>
-                    <IconButton>
-                        <AttachFile/>
-                    </IconButton>
-                    <IconButton>
-                        <MoreVert/>
-                    </IconButton>
-                </div>
-             </div>
-            <div className="chat__body">
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 500));
+  }, []);
 
-                {messages.map((message) => (
-                    <p className={`chat__message ${message.name === user.displayName && 'chat__reciever'} `}>
+  useEffect(() => {
+    if (input.length > 0) {
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("typing")
+        .doc(user.displayName)
+        .set({ lastEdit: Date.now() });
 
-                        <span className="chat__name" >
-                            {message.name}
-                        </span>
-                        {message.message}
-                        <span className="chat__timestamp" >
-                            {new Date(message.timestamp?.toDate()).toUTCString()}
-                        </span>
-                    </p>
-                ))}
+      setIdleWatch(setTimeout(removeTyping, 10000));
+    } else {
+      removeTyping();
+    }
 
-                
-                    <div>
-                         {isTyping ? (
-                            <div className= {`chat__message ${messages[messages.length - 1].name === user.displayName && 'chat--invisible'} `} >
-                            {user.displayName} is Typing...
-                            </div>
-                   ) : (<div></div>)}
-                    </div>
-                
+    // db.collection('rooms').doc(roomId)
+    //   .collection('messages')
+    //   .orderBy('timestamp', 'desc').limit(2)
+    //   .onSnapshot(snapshot => (
+    //     snapshot.docs.map((doc) => {
+    //         console.log(doc.id);
+    //         if (doc.data().typing === true) {
+    //             setTyping(true);
+    //         }
+    //         if (input === '' ) {
+    //             db.collection('rooms')
+    //             .doc(roomId)
+    //             .collection('messages')
+    //             .doc(doc.id).update({ typing: "false"  })
+    //             setTyping(false);
+    //         }
 
-            </div>
-            <div className="chat__footer">
-                <InsertEmoticon/>
-                <form>
-                    <input value={input} onChange={e => {
-                        setInput(e.target.value)
-                        
+    //     } )
+    // ));
 
+    db.collection("rooms")
+      .doc(roomId)
+      .collection("messages")
+      .doc("7LGChzUW5OvbTRLHr2Fy")
+      .set({ name: "true" });
 
+    //   .orderBy('timestamp', 'desc').limit(1).id );
+    //   .onSnapshot(snapshot => (
+    //       snapshot.docs.map((doc) => {  doc.data() ; console.log ( doc.set({ typing: false }))}  )
 
+    // ));
 
+    console.log(input);
+    // db.collection('rooms')
+    //   .doc(roomId)
+    //   .collection('typing')
+    //   .doc('VjntTj6WAt2Iqr6H7Jbs').set({ flag: true  })
 
+    // db.collection('rooms').doc(roomId).collection('typing').onSnapshot(snapshot => {
+    //     snapshot.docs.map(doc => (
+    //      //  console.log(doc.data().flag),
+    //        setTyping(doc.data().flag),
+    //       console.log(doc.data().flag)
+    //     ))
+    // })
 
+    // console.log("input is changing");
+    // console.log(typing);
 
-                    }} placeholder="Type a message..." type="text"></input>
-                    <button onClick={sendMessage} type="submit"></button>
-                </form>
-                <Mic/>
-            </div>
+    // if (input === '') {
+
+    // console.log('input is empty');
+    // console.log(typing);
+
+    // db.collection('rooms')
+    //   .doc(roomId)
+    //   .collection('typing')
+    //   .doc('VjntTj6WAt2Iqr6H7Jbs').set({ flag: false  })
+
+    //   db.collection('rooms').doc(roomId).collection('typing').onSnapshot(snapshot => {
+    //     snapshot.docs.map(doc => (
+    //     //    console.log(doc.data().flag),
+    //        setTyping(doc.data().flag)
+    //     ))
+
+    //console.log(typing)
+    // })
+    // }
+
+    // db.collection('rooms').doc(roomId)
+    //       .collection('messages').add({
+    //         message: input,
+    //         name: user.displayName,
+    //         typing: false,
+    //         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    //       });
+  }, [input]);
+
+  useEffect(() => {
+    return () => {
+      if (idleWatch) {
+        clearTimeout(idleWatch);
+      }
+    };
+  }, [idleWatch]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(input);
+
+    db.collection("rooms").doc(roomId).collection("messages").add({
+      message: input,
+      name: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar
+          src={`https://avatars.dicebear.com/4.5/api/human/${seed}.svg`}
+        />
+        <div className="chat__headerInfo">
+          <h3>{roomName}</h3>
+          <p>
+            {" "}
+            Last seen &nbsp;
+            {new Date(
+              messages[messages.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
-    );
+        <div className="chat__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${
+              message.name === user.displayName && "chat__reciever"
+            } `}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
+
+        <div className="chat__message">
+          {typing.length > 0 && typing.join(", ") + " is typing..."}
+        </div>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            placeholder="Type a message..."
+            type="text"
+          ></input>
+          <button onClick={sendMessage} type="submit"></button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,75 +1,79 @@
-import { Avatar, IconButton } from '@material-ui/core';
-import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import React, { useEffect, useState } from 'react';
-import './Sidebar.css';
-import SidebarChat from './SidebarChat';
-import db from './firebase';
-import { useStateValue } from './StateProvider';
+import { Avatar, IconButton } from "@material-ui/core";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import React, { useEffect, useState } from "react";
+import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
+import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 import firebase from "firebase";
-import { actionTypes } from './reducer';
+import { actionTypes } from "./reducer";
 
 function Sidebar(props) {
+  const [rooms, setRooms] = useState([]);
+  // const [signedOut, setSignOut] = useState();
+  const [{ user }, dispatch] = useStateValue();
 
-    const [rooms, setRooms] = useState([]);
-    // const [signedOut, setSignOut] = useState();
-    const [{ user }, dispatch] = useStateValue();
+  const signOutButtonPressed = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        // Sign-out successful.
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: null,
+        });
+      })
+      .catch(function (error) {
+        // An error happened.
+      });
+  };
 
-    const signOutButtonPressed = () => {
-        firebase.auth().signOut().then(function() {
-            // Sign-out successful.
-            dispatch({
-                type: actionTypes.SET_USER,
-                user: null,
-            });
-          }).catch(function(error) {
-            // An error happened.
-          });
-    }
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      const rooms = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }));
+      setRooms(rooms);
+    });
+  }, []);
 
-    useEffect(() => {
-        db.collection('rooms').onSnapshot(snapshot => {
-            setRooms(snapshot.docs.map(doc => (
-                {
-                    id: doc.id,
-                    data: doc.data()
-                }
-            )))
-        })
-    }, []);
-
-    return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                <Avatar src={user?.photoURL} />
-                <div className="sidebar__headerRight">
-                    <IconButton>
-                        <DonutLarge/>                        
-                    </IconButton>
-                    <IconButton>
-                        <Chat/>
-                    </IconButton>
-                    <IconButton>
-                        <ExitToAppIcon onClick={signOutButtonPressed} />
-                    </IconButton>
-                </div>    
-            </div>  
-            <div className="sidebar__search">
-                <div className="sidebar__searchContainer">
-                    <SearchOutlined/> 
-                    <input className="sidebar__input" placeholder="Search or start new chat" type="text"/>
-                </div>
-            </div>  
-            <div className="sidebar__chats">
-                <SidebarChat addNewChat/>    
-                {
-                    rooms.map(room => (
-                        <SidebarChat key={room.id} id={room.id} name={room.data.name}/>
-                    ))
-                } 
-            </div>  
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src={user?.photoURL} />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon onClick={signOutButtonPressed} />
+          </IconButton>
         </div>
-    );
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input
+            className="sidebar__input"
+            placeholder="Search or start new chat"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat addNewChat />
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
